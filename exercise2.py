8f82d8f4940222efbc6c5e7b8ee25bdf835b0738
Mohammad Reza Haji Hosseini <80394968+Mohammadrezahajihosseini@users.noreply.github.com>
2021-10-29 18:46:05 +0200

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -73,7 +73,7 @@ def find_token_silver():#function to find silver tokens
     if silver_dist==100:
      return -1, -1
     else:
-     return silver_dist, silver_rot_y,
+     return silver_dist, silver_rot_y
 
 def find_token_golden():#function to find golden tokens:
 
@@ -85,7 +85,7 @@ def find_token_golden():#function to find golden tokens:
             golden_dist=token.dist
             golden_rot_y=token.rot_y
       
-    if silver_dist==100:
+    if golden_dist==100:
      return -1, -1
     else:
      return golden_dist, golden_rot_y
